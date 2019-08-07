dbc784b319251e24a88ef7aa4943d46c65548a9b
Edeviatov <edeviatov@htc-cs.ru>
2019-08-07 10:38:55 +0400

    Initial Commit

---
+++ webviewAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webviewAPP
+//
+//  Created by Egor Devyatov on 07.08.2019.
+//  Copyright © 2019 Egor Devyatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
